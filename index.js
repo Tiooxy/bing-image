f5ce4c5c4853038f3e4f7969baf46802c585bd78
Tioo <153548133+Tiooxy@users.noreply.github.com>
2024-03-17 13:12:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,47 +2,19 @@ const express = require('express')
 const router = express.Router();
 const FormData = require('form-data');
 const {
-  BingImageCreator
+  igstalk
 } = require('./skrep.js');
 const app = express();
 const port = 8080;
 
-router.get('/createImage', async (req, res) => {
-  try {
-    let text = req.query.prompt;
+router.get('/tiktok-stalk', async (req, res) => {
+  let q = req.query.text
+  if (!q) return res.json('Masukan username')
+  let result = await igstalk(q)
+  res.header('Content-Type: application/json')
+  res.type('json').send(JSON.stringify(result, null, 2))
+})
 
-if (!text) {
-      return res.json({
-        error: 'Parameter tidak valid, gunakan /bing-img?prompt=<teks>'
-      });
-    }
-
-const bingImageCreator = new BingImageCreator({
-      cookie: '14cRmMi8F7BapABkfSsK8gkOJ1fGfTBFPSQEo934atFVfD8kSRXaqs5HktCx3ytMkmGeRgX5AZeQbtIgqu2fOsDBX7ywz8_lERjX0PNZQ4PEKb9sa5JF2_-eSvFE_QRyeUljYpUregF7Il9O-9xmoEV1-geLS3JSZa7Zx1gJRhXkdo3b6oyTRvh1R0yy32u-0DFR8TN9Tmf_xUuNEm1ynIw'
-    });
-
-const data = await bingImageCreator.createImage(text);
-
-if (data.length > 0) {
-      res.json({
-        success: true,
-        author: 'Tio',
-        result: data
-      });
-    } else {
-      res.json({
-        success: false,
-        author: 'Tio',
-        message: 'Prompt mungkin telah diblokir gunakan prompt lain'
-      });
-    }
-  } catch (error) {
-    console.error(`Kesalahan API: ${error.message}`);
-    res.json({
-      error: 'Kesalahan server internal'
-    });
-  }
-});
 
 app.use('/', router);
 
